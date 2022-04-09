222d7658f58859435f5c73c97f833268ffb2dcbf
jiaqianjing <jiaqianjing1992@163.com>
2022-04-09 18:24:38 +0800

    Update start_gitlab_repo.sh

--- start_gitlab_repo.sh
+++ start_gitlab_repo.sh
@@ -2,7 +2,7 @@
 local_ip=`hostname -i` # this command only support linux
 docker run -d \
   -p $local_ip:443:443 \
-  -p $local_ip:2022:2022 \ # 这里 2022 是容器内 ngix 代理的 gitlab 端口，需要在 /etc/gitlab/gitlab.rb 中设置 external_url
+  -p $local_ip:2022:2022 \ # 这里 2022 是容器内 ngix 代理的 gitlab 地址，在配置文件 gitlab/gitlab.rb 中 根据宿主机的情况修改 external_url
   -p $local_ip:2222:22 \
   -v $PWD/gitlab/config:/etc/gitlab \
   -v $PWD/gitlab/logs:/var/log/gitlab \
